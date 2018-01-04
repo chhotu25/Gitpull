6bf0be28da3a454d2cac3a0b9afe856e6c1d8208
AL Moin Webtech <AL Moin Webtech@AMW-G1>
2018-01-04 15:47:53 +0530

    First test.

---
+++ DemoGit/src/samplePull/SampleData.java
@@ -0,0 +1,11 @@
+package samplePull;
+
+public class SampleData {
+int data[]={1, 2,42};
+	
+	public void display(){
+		System.out.println("Data: data test range");
+	}
+	
+	
+}
---
+++ DemoGit/src/samplePull/Test1.java
@@ -0,0 +1,17 @@
+package samplePull;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		SampleData data =new SampleData();
+		displayMessage();
+		data.display();
+	}
+	private static void displayMessage(){
+		
+  System.out.println("Hello test program");
+  System.out.println("This test on git repos");
+	}
+
+}
+	
